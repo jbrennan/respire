023acbc180815b13440ec8e0b67f11d009de0570
Jason Brennan <i.jasonbrennan@gmail.com>
2012-12-02 13:12:42 -0500

    Initial Commit

---
+++ Respire/JBAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  JBAppDelegate.m
+//  Respire
+//
+//  Created by Jason Brennan on 2012-12-02.
+//  Copyright (c) 2012 Jason Brennan. All rights reserved.
+//
+
+#import "JBAppDelegate.h"
+
+@implementation JBAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ Respire/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Respire
+//
+//  Created by Jason Brennan on 2012-12-02.
+//  Copyright (c) 2012 Jason Brennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
